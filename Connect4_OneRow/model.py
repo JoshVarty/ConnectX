498e15872b450aa8cf22a328777fe0dbd2b947cc
Josh Varty <joshvarty@gmail.com>
2020-04-05 22:48:24 -0400

    Add predict() to model

--- Connect4_OneRow/model.py
+++ Connect4_OneRow/model.py
@@ -1,12 +1,16 @@
+import numpy as np
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Connect4_OneRow_Model(nn.Module):
 
-    def __init__(self, game):
+    def __init__(self, game, device):
 
         super(Connect4_OneRow_Model, self).__init__()
 
+        self.device = device
         self.size = game.get_board_size()
         self.action_size = game.get_action_size()
 
@@ -26,3 +30,14 @@ class Connect4_OneRow_Model(nn.Module):
         value_logit = self.value_head(x)
 
         return F.log_softmax(action_logits, dim=1), F.tanh(value_logit)
+
+
+    def predict(self, board):
+        board = torch.FloatTensor(board.astype(np.float32))
+        board.to(self.device)
+        board = board.view(1, self.size)
+        self.eval()
+        with torch.no_grad():
+            pi, v = self.forward(board)
+
+        return torch.exp(pi).data.cpu().numpy()[0], v.data.cpu().numpy()[0]
