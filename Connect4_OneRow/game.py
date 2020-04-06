509ef6f405408c8e42a721759e992af5b7595e37
Josh Varty <joshvarty@gmail.com>
2020-04-05 22:27:22 -0400

    Add game class

---
+++ Connect4_OneRow/game.py
@@ -0,0 +1,72 @@
+import numpy as np
+from Connect4_OneRow.board import Board
+
+class Connect3OneRowGame:
+    """
+    A very, very simple game of ConnectX in which we have:
+        rows: 1
+        columns: 4
+        winNumber: 2
+    """
+
+    def __init__(self):
+        self.columns = 4
+
+    def get_init_board(self):
+        b = Board()
+        return np.array(b.pieces)
+
+    def get_board_size(self):
+        return self.columns
+
+    def get_action_size(self):
+        return self.columns
+
+    def get_next_state(self, board, player, action):
+        b = Board(board)
+        move = (int(action))
+        b.execute_move(move, player)
+
+        # Return the new game, but
+        # change the perspective of the game with negative
+        return (b.pieces, -player)
+
+    def get_valid_moves(self, board, player):
+        # All moves are invalid by default
+        valid_moves = [0] * self.get_action_size()
+        b = Board(board)
+
+        legalMoves = b.get_legal_moves(player)
+        if len(legalMoves) == 0:
+            # No valid moves
+            return valid_moves
+
+        # Mark all legal moves
+        for legalIndex in legalMoves:
+            valid_moves[legalIndex] = 1
+
+        return valid_moves
+
+    def get_game_ended(self, board, player):
+        # return 0 if not ended, 1 if player 1 wins, -1 if player 1 lost
+
+        b = Board(board)
+
+        if b.is_win(player):
+            return 1
+        if b.is_win(-player):
+            return -1
+        if b.has_legal_moves():
+            return 0
+
+        # Draws have very little (but positive reward)
+        return 1e-4
+
+    def get_canonical_form(self, board, player):
+        return player * board
+
+    def string_representation(self, board):
+        return board.tostring()
+
+
+
