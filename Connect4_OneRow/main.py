6624ef89c880f06fb98bc67f4bd5a25f6d6cc4d1
Josh Varty <joshvarty@gmail.com>
2020-04-07 22:55:05 -0400

    Start visualizing counts

--- Connect4_OneRow/main.py
+++ Connect4_OneRow/main.py
@@ -12,11 +12,10 @@ from Connect4_OneRow.trainer import Trainer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
 args = {
     'batch_size': 64,
-    'numIters': 25,
-    'numEps': 100,              # Number of full games (episodes) to run during each iteration
+    'numIters': 20,
+    'numEps': 50,              # Number of full games (episodes) to run during each iteration
     'tempThreshold': 15,        # Number of iterations before we switch temp from 1 to 0
     'numItersForTrainExamplesHistory': 20,
     'updateThreshold': 0.6,     # Percentage wins required against previous model required in order to update model
@@ -30,6 +29,9 @@ def generateStates(board=None, player=1, prefix=1):
         board = Board()
 
     current_state = board.tostring()
+
+    if current_state in lookup:
+        state_visit_count = current_state
 
     if not graph.has_node(current_state):
         if board.is_win(1):
@@ -70,14 +72,41 @@ action_size = game.get_action_size()
 model = Connect4_OneRow_Model(board_size, action_size, device)
 
 trainer = Trainer(game, model, args)
-#trainer.learn()
+trainer.learn()
 
 # Now that we've got a trained model, let's visualize the tree
+lookup = {}
+for pieces, policy, value in trainer.all:
+    board = Board(pieces)
+    state_string = board.tostring()
+
+    if state_string not in lookup:
+        lookup[state_string] = 0
+
+    lookup[state_string] = lookup[state_string] + 1
+
+
+
 generateStates()
 
 plt.title('draw_networkx')
 pos=graphviz_layout(graph, prog='dot')
+nodes = graph.nodes
+
+l = {}
+
 nx.draw(graph, pos, with_labels=True, arrows=True, node_color=color_map)
+
+for node in graph.nodes:
+    if node in lookup:
+        l[node] = str(lookup[node])
+    else:
+        l[node] = ""
+
+    y,x = pos[node]
+    pos[node] = (y + 10, x + 15)
+
+nx.draw_networkx_labels(graph, pos=pos, font_size=8, font_color='black', labels=l)
 
 plt.show()
 
